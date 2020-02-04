2e0b2d487ce16d7c601a58a4322e0f054d96ac48
Ahsan_Ali <mr.ahsanali@outlook.com>
2020-02-04 11:08:14 +0500

    Create navbar.dart

---
+++ lib/elements/navbar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NavBar extends StatefulWidget {
+  NavBar({Key key}) : super(key: key);
+  int initialIndex;
+  @override
+  _NavBarState createState() => _NavBarState();
+}
+
+class _NavBarState extends State<NavBar>  with SingleTickerProviderStateMixin {
+  TabController _tabController;
+  
+  @override
+  void @override
+  void initState() {
+    _tabController = TabController(
+      length: 3, vsync: this, initialIndex: widget.initialIndex
+    );
+    super.initState();
+    
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: child,
+    );
+  }
+}
